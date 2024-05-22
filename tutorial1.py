d8de6f9bdd5c03a6305798f623a1b45e9ea7771d
Aayushman Amatya <aayushmannp@gmail.com>
2024-05-22 20:43:34 +0545

    my first time programming in python

---
+++ tutorial1.py
@@ -0,0 +1,42 @@
+#Write a script that adds 15 and 30 and divides the sum by 2.Print out the answer.
+print((15+30)/2)
+
+#Initialize two variables and use arithmetic operators to add,substract,divide and find the remainder.
+num1=20
+num2=21
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1%num2)
+
+#Create a variable called name and store your name in it as a string.
+name="Aayushman Amatya"
+print(name)
+
+#Create three variables in one line and assign each one a different food item.
+akhilesh,aryan,aabish="Fried Rice","Gimbap","Rice"
+print(akhilesh+" "+aryan+" "+aabish)
+
+#Print out "Hello" ten times using arithmetic operators.
+print("Hello"*10)
+
+#Create two stringss and then create a third variable combining both of the two original strings.
+sent1="Aayushman"
+sent2="Amatya"
+sent3=sent1+" "+sent2
+print(sent3)
+
+#Create a string and print the fourth letter of the word.
+sent="structure"
+print(sent[3])
+
+#Create a string and print the letters from index 0 to 5.
+fname="AtishThapa"
+print(fname[0:5])
+
+#Create a variable and print all the letters from the first index until the end.
+letter="Hello"
+print(letter)
+
