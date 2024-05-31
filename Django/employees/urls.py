9d0f8899a68032ae5e17b3502a228f2dcd5ff042
Aayushman4725 <aayushmannp@gmail.com>
2024-05-31 22:08:12 +0545

    updated views.py to show employee/student details and updated urls.py

--- Django/employees/urls.py
+++ Django/employees/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'employees'
 urlpatterns = [
-    path('<int:pk>/',views.employee_detail),
+     path('student/<int:pk>/', views.student_detail, name='student_detail'),
+    path('employee/<int:pk>/',views.employee_detail,name='employee_detail'),
+   
 
 ]
--- Django/employees/views.py
+++ Django/employees/views.py
@@ -1,9 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from employees.models import Employee
+from employees.models import Employee, Student
 
 # Create your views here.
 
 def employee_detail(request, pk):
     employee = get_object_or_404(Employee,pk=pk)
-    return HttpResponse(employee)
+    context ={
+        'employee' : employee,
+    }
+    return render(request, 'employee_detail.html',context)
+
+def student_detail(request, pk):
+    student = get_object_or_404(Student,pk=pk)
+    context= {
+        'student' : student,
+    }
+    return render(request, 'student_detail.html',context)
